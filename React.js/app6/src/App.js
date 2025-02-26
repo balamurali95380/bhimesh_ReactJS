bd3dedd2ab0c0a8133c671ac05caf4e305538cd7
balamurali95380 <balamurali95380@gmail.com>
2025-02-26 12:53:00 +0530

    app6

---
+++ React.js/app6/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>this is react js</h2>
+    <p>this is app6</p>
+    </div>
+  );
+}
+
+export default App;
