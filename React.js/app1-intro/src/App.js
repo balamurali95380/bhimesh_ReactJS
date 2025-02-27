e176cf6d5a3e80b2caab172e5902e8406abc8d28
balamurali95380 <balamurali95380@gmail.com>
2025-02-27 10:47:48 +0530

    app1ul

--- React.js/app1-intro/src/App.js
+++ React.js/app1-intro/src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
     <h2>this is react js</h2>
     <p>working in progress app1</p>
+    <ul>
+       <li>AP</li>
+                    <li>HP</li>
+                    <li>GOA</li>
+                    <li>Pune</li>
+                </ul>
     </div>
   );
 }
