fa2417816bec632ebfc1dec2240988ba222bc921
balamurali95380 <balamurali95380@gmail.com>
2025-02-27 10:51:41 +0530

    app1form

--- React.js/app1-intro/src/App.js
+++ React.js/app1-intro/src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h2>this is react js</h2>
-    <p>working in progress app1</p>
-    <ul>
-       <li>AP</li>
-                    <li>HP</li>
-                    <li>GOA</li>
-                    <li>Pune</li>
-                </ul>
+      <h2>this is react js</h2>
+      <p>working in progress app1</p>
+      <ul>
+        <li>AP</li>
+        <li>HP</li>
+        <li>GOA</li>
+        <li>Pune</li>
+      </ul>
     </div>
   );
 }
