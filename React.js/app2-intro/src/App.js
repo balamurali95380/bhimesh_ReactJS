972aabf382f6f9bdfcbcf8684dac55f76ffeaed2
balamurali95380 <balamurali95380@gmail.com>
2025-02-27 10:56:31 +0530

    app2form

--- React.js/app2-intro/src/App.js
+++ React.js/app2-intro/src/App.js
@@ -6,6 +6,20 @@ function App() {
     <div className="App">
     <h2>this is react js</h2>
     <p>working in progress app2</p>
+    <label for="">First Name:</label>
+          <input type="text" />
+        
+          <label for="">Last Name:</label>
+          <input type="text" />
+          
+          <label for="">DateOfBirth: </label>
+          <input type="date"  />
+          
+          <label for="">Email ID: </label>
+          <input type="text" readonly />
+          
+          <label for="">Mobilenumber: </label>
+          <input type="number" />
     </div>
   );
 }
