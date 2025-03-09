853230c1ca1ae613c40ef234d50e8b0b4797334b
balamurali95380 <balamurali95380@gmail.com>
2025-03-09 10:13:20 +0530

    crudprac23

---
+++ React.js/app11_crud1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Formex from './Formex';
+
+function App() {
+  return (
+    <div className="App">      
+      <Formex/>
+    </div>
+  );
+}
+
+export default App;
