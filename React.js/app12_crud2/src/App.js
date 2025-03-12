d9859eeca02d563a9886f7d550ef4032f42153d8
balamurali95380 <balamurali95380@gmail.com>
2025-03-12 11:28:25 +0530

    api3

--- React.js/app12_crud2/src/App.js
+++ React.js/app12_crud2/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 // import Crud2 from './Crud2';
-import Crud3 from './Crud3';
+// import Crud3 from './Crud3';
+import Api_server from './Api_server';
 
 function App() {
   return (
     <div className="App">
       {/* <Crud2/> */}
-      <Crud3/>
+      {/* <Crud3/> */}
+      <Api_server/>
     </div>
   );
 }
