b14edd223cd082d32d4175f3e843fbaa38f84e36
balamurali95380 <balamurali95380@gmail.com>
2025-03-13 05:30:02 +0530

    mini2

---
+++ React.js/apphooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HookA from './HookA';
+
+function App() {
+  return (
+    <div className="App">
+      <HookA/>
+    </div>
+  );
+}
+
+export default App;
