1ea931e099297ec1a1430cd746ec46abad6ef46a
balamurali95380 <balamurali95380@gmail.com>
2025-03-20 09:23:26 +0530

    newapp

---
+++ React.js/app-prj/src/Cls.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class Cls extends Component{
+    constructor(){
+        super()
+        this.state={
+            person:[]
+        }
+    }
+    render(){
+        return <div><h1>Cls class</h1></div>
+    }
+}
