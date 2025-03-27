ea6ee11ec7617c718d56896f700c9a910fd1fede
balamurali95380 <balamurali95380@gmail.com>
2025-03-27 15:37:04 +0530

    render3

---
+++ React.js/contapi/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Par1 from './Par1'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Par1/>
+    </>
+  )
+}
+
+export default App
