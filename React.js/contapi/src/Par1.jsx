37793933bb9582f7f29ecc02f3b5e5be35a20402
balamurali95380 <balamurali95380@gmail.com>
2025-03-28 08:44:16 +0530

    string

---
+++ React.js/contapi/src/Par1.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import { Usercontextprovider } from "./Cntapi";
+import Chi1 from "./Chi1";
+
+export default class Par1 extends Component{
+    constructor(){
+        super()
+        this.state={
+            msg1:"hi parent",
+            mag2:"from parent",
+        }
+    }
+    render(){
+
+        return(
+            <div>
+                <h1>FROM PARENT1</h1>
+                <Usercontextprovider value={this.state.msg2} />
+                <Chi1   msg1={this.state.msg1}/>
+                 <Usercontextprovider />
+            </div>
+        )
+    }
+}
