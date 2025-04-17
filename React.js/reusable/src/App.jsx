3f89676d0149a943af76d096d2866ae79d908b77
balamurali95380 <balamurali95380@gmail.com>
2025-04-17 09:41:42 +0530

    redux pract2

--- React.js/reusable/src/App.jsx
+++ React.js/reusable/src/App.jsx
@@ -5,13 +5,22 @@ import './App.css'
 
 import Users from './Store/Users'
 
+import Employees from './Store/Employees'
+import Employee from './Store/Employee'
+import Employee1 from './Store/Employee1'
+import Employeecrud from './Store/Employeecrud'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    {/* <Employees/> */}
+    {/* <Employee/> */}
+    {/* <Employee1/> */}
+    <Employeecrud/>
 
-    <Users/>
+    {/* <Users/> */}
    
     </>
   )
