7abf2229a54ad8a43a30071cf4951e6e5e682f27
balamurali95380 <balamurali95380@gmail.com>
2025-05-19 08:33:05 +0530

    contexttodo

--- ReactJsProjects/alltopics4/src/App.jsx
+++ ReactJsProjects/alltopics4/src/App.jsx
@@ -2,32 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './Dashboard'
+import TodoApp from './TodoApp'
+import { UserContext } from './UserContext'
+import Profile from './Profile'
 
 function App() {
   const [count, setCount] = useState(0)
+  const user = {
+    name: 'Alice Johnson',
+    email: 'alice@example.com'
+  };
 
   return (
     <>
+    <Dashboard/>
+    <TodoApp/>
+     <UserContext.Provider value={user}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Welcome to the App</h1>
+        <Profile />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </UserContext.Provider>
     </>
   )
 }
