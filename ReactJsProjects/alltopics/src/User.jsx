d8069da4e4ad2dea06ca5b8b10a2c5b19c64fd11
balamurali95380 <balamurali95380@gmail.com>
2025-05-20 08:22:25 +0530

    json

--- ReactJsProjects/alltopics/src/User.jsx
+++ ReactJsProjects/alltopics/src/User.jsx
@@ -1,47 +1,45 @@
-// import { Component } from "react"
+import { Component } from "react"
+import axios from "axios"
 
-// export default class User extends Component{
-//     constructor(){
-//         super()
-//         this.state={
-//             person:{
-//                 fname:"bhimesh",
-//             },
-//             User:[],
-//     }}
-//     getuserdata=()=>{
+export default class User extends Component{
+    constructor(){
+        super()
+        this.state={
+            person:{
+                fname:"bhimesh",
+            },
+            User:[],
+    }}
+    getuserdata=()=>{
      
-//         axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-//             this.setState({ User: response.data })
-//         })
-//     }
-
-//     }
-//     // render(){
-//     //     return (<div>               <h1>{this.state.person.fname}</h1>
-//     //     <table>
-//     //     <thead>
-//     //             <tr>
-//     //                 <th>name</th>
-                   
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {this.state.User.map((std, i) => {
-//                     return (<tr key={i}>
-//                         <td>{std.name}</td>
-                   
-//                     </tr>
-//                     )
-//                 })}
-
-//             </tbody>
-
-//         </table>
-            
-//             </div>)
+        axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+            this.setState({ User: response.data })
+        })
+    }
+    render(){
+        return<>
+        <table>
+            <thead>
+                <tr>
+                    <th>name</th>
+                </tr>
+               
+            </thead>
+            <tbody>
+                {this.state.User.map((usr,i)=>{
+                    return(<tr key={i}>
+                        <td>{usr.name}</td>
+                        </tr>)
+                    })}
+              
+            </tbody>
+        </table>
         
-//     }
-// // componentdid(){
-// //         this.getuserdata()
-// //     }
+        </>
+    }
+    componentDidMount(){
+        this.getuserdata()
+    }
+
+    }
+
